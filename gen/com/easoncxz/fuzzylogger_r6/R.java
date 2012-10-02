f057dc525e1d886f00b87f51635ab149fe6dcf83
easoncxz <me@easoncxz.com>
2012-10-02 10:44:16 +0800

    deleted irrelevant drawables.

--- gen/com/easoncxz/fuzzylogger_r6/R.java
+++ gen/com/easoncxz/fuzzylogger_r6/R.java
@@ -11,9 +11,7 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int noun_project_3247_transparent=0x7f020002;
+        public static final int noun_project_3247_transparent=0x7f020000;
     }
     public static final class id {
         public static final int home_btnRecord=0x7f070000;
